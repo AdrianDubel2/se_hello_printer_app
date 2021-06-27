5c1973f2d52848b408398459553a1014361c14ad
AdrianDubel2 <greatparty.sale@gmail.com>
2021-06-27 10:17:02 +0000

    poprawka2

--- test/test_views.py
+++ test/test_views.py
@@ -3,6 +3,7 @@ import json
 from hello_world import app
 from hello_world.formater import SUPPORTED
 
+
 class FlaskrTestCase(unittest.TestCase):
     def setUp(self):
         app.config['TESTING'] = True
